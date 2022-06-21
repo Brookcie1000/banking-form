805c75e43971df248ba5ee33d287ba32c010b3fb
James Brooks <jamescbrooks2020@gmail.com>
2022-06-21 13:48:24 +1000

    Basic Layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+import { Box, Card } from '@mui/material';
 import './App.css';
+import Onboarding from './components/onboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Onboarding></Onboarding>
     </div>
   );
 }
