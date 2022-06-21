caafb0d33b9ae83cb70d83a3b10084064c0d1e78
James Brooks <jamescbrooks2020@gmail.com>
2022-06-21 13:58:32 +1000

    Remove Clutter

--- src/components/onboard.js
+++ src/components/onboard.js
@@ -12,16 +12,15 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Card, Paper } from '@mui/material';
-import { PictureAsPdf, PictureAsPdfRounded } from '@mui/icons-material';
-import { grid } from '@mui/system';
+import { Paper } from '@mui/material';
+import { PictureAsPdfRounded } from '@mui/icons-material';
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        Banking Co.
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -50,7 +49,7 @@ export default function Onboarding() {
             marginTop: 8,
             display: 'flex',
             flexDirection: 'column',
-            alignItems: 'center',
+            alignItems: 'center'
         }}
         >
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
@@ -82,9 +81,17 @@ export default function Onboarding() {
                             </Button>
                         </Grid>
                         <Grid item xs={12}>
+                            <Button>
+                                <Typography>
+                                    Company Policies
+                                </Typography>
+                                <PictureAsPdfRounded></PictureAsPdfRounded>
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12}>
                             <FormControlLabel
                             control={<Checkbox value="allowExtraEmails" color="primary" />}
-                            label="I confirm I have read and agree with my employment contract."
+                            label="I confirm I have read and agree with my employment contract and company polices."
                             />
                         </Grid>
                     </Grid>
@@ -133,8 +140,29 @@ export default function Onboarding() {
                         <TextField
                         required
                         fullWidth
+                        id="email"
+                        label="Confirm Email Address"
+                        name="email"
+                        autoComplete="email"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        required
+                        fullWidth
                         name="password"
                         label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Confirm Password"
                         type="password"
                         id="password"
                         autoComplete="new-password"
@@ -147,7 +175,7 @@ export default function Onboarding() {
                     variant="contained"
                     sx={{ mt: 3, mb: 2 }}
                     >
-                    Sign Up
+                    Complete Onboarding Sign Up
                     </Button>
                     <Grid container justifyContent="flex-end">
                     <Grid item>
